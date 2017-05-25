75e2b0e21a1aed4340eedea64800c3810c0dce63
Pulipati Narasimha <narasimha.pulipati@nokia.com>
2017-05-25 14:24:33 +0530

    REG-1234
    Change-Id: I48f357443acdb4c4673d874a0fe3360274b289e6

---
+++ interrupt_delta.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import subprocess
+import optparse
+
+parser = optparse.OptionParser()
+parser.add_option('-d', '--dev', dest='dev', help='Device Name')
+
+(options, args) = parser.parse_args()
+
+print options.dev
+
+command = "cat /proc/interrupts | grep " + options.dev + " | awk -F ' ' '{s+=$2+$2+$3+$4+$5+$6+$7+$9} END {print s}'"
+
+prev_out = 0
+while 1:
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+        (output, err) =  p.communicate()
+        delta = int(output) - prev_out
+        prev_out = int(output)
+        print "interrupts generated during this time " , delta
+        subprocess.call(["sleep", "1"])
