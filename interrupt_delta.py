a8d9e6afe8d4580078e2168d09a16d3ed72d420e
Pulipati Narasimha <narasimha.pulipati@nokia.com>
2017-05-30 23:52:45 +0530

    REG-1234
    Change-Id: If59664815181121b366c12ad4fb3c5d511898674

--- interrupt_delta.py
+++ interrupt_delta.py
@@ -10,13 +10,25 @@ parser.add_option('-d', '--dev', dest='dev', help='Device Name')
 
 print options.dev
 
-command = "cat /proc/interrupts | grep " + options.dev + " | awk -F ' ' '{s+=$2+$3+$4+$5+$6+$7+$9} END {print s}'"
+dia = "cat /proc/interrupts | grep virtio2-in | awk -F ' ' '{s+=$2+$3+$4+$5+$6+$7+$9} END {print s}'"
+icm = "cat /proc/interrupts | grep virtio0-in | awk -F ' ' '{s+=$2+$3+$4+$5+$6+$7+$9} END {print s}'"
 
-prev_out = 0
-while 1:
+prev_dia = 0
+prev_icm = 0
+op = 0
+
+def intr_delta(command):
         p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
         (output, err) =  p.communicate()
-        delta = int(output) - prev_out
-        prev_out = int(output)
-        print "interrupts generated during this time " , delta
+        return int(output)
+
+
+while 1:
+        op = intr_delta(dia)
+        dia_delta = op - prev_dia
+        prev_dia = op
+        op = intr_delta(icm)
+        icm_delta = op - prev_icm
+        prev_icm = op
+        print " INTR DIA " , dia_delta, " INTR ICM ", icm_delta
         subprocess.call(["sleep", "1"])
