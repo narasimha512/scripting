f2f5ff5ad91111ff320af2da9acaaae09ba3c63b
Pulipati Narasimha <narasimha.pulipati@nokia.com>
2019-10-01 16:28:16 +0530

    UDMT-99: scripts
    Change-Id: I535b70b76080b1ad050a44c142b5a6700dc689bb

--- netstat.sh
+++ netstat.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-while [ TRUE ]
-do
+log_netstat()
+{
+PORT=$1
 NOW=$(date +"%m-%d-%Y-%H-%M")
-FILE="//var/opt/sdf/$NOW.log"
+FILE="$2/$NOW.log"
 NOW_SEC=$(date +"%m-%d-%Y-%H-%M-%S")
 echo "Time: $NOW_SEC" >> $FILE
-netstat -anp | grep 16611 >> $FILE
+RQ=$(expr $(netstat -anp | grep $PORT| awk -F ' ' '{s+=$2} END {print s}') / 1000)
+SQ=$(expr $(netstat -anp | grep $PORT| awk -F ' ' '{s+=$3} END {print s}') / 1000)
+echo "RQ $RQ KB SQ $SQ KB" >> $FILE
+netstat -anp | grep $PORT >> $FILE
+}
+
+while [ TRUE ]
+do
+log_netstat 16611 /data0/16611/
+log_netstat 8080 /data0/8080/
 sleep 1
 done
